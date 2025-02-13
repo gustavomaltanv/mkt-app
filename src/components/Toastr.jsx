d57ffb0c8789aecb79e96728b726d69314bf8d65
gustavo-dk <gustavomaltanv@gmail.com>
2025-02-12 21:24:43 -0300

    fix: correção do Toastr

--- src/components/Toastr.jsx
+++ src/components/Toastr.jsx
@@ -1,21 +1,20 @@
 import toastr from 'toastr';
 
 toastr.options = {
-  closeButton: true,
-  debug: false,
-  newestOnTop: false,
-  progressBar: true,
-  positionClass: 'toast-top-right',
-  preventDuplicates: false,
-  onclick: null,
-  showDuration: '300',
-  hideDuration: '1000',
-  timeOut: '5000',
-  extendedTimeOut: '1000',
-  showEasing: 'swing',
-  hideEasing: 'linear',
-  showMethod: 'fadeIn',
-  hideMethod: 'fadeOut',
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": true,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": true,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
 };
 
 export function mostrarMensagem(titulo, mensagem, tipo) {
