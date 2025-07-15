9bdd469a159dbac6adfe5816e50d0dca76b80884
gustavo-nt <gustavomaltanv@gmail.com>
2025-07-15 08:07:03 -0300

    feat: atualizar biblioteca bootswatch e altera tema

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "bootswatch/dist/zephyr/bootstrap.min.css"
+import "bootswatch/dist/brite/bootstrap.min.css"
 import { rotas } from "./rotas"
 import { RouterProvider } from "react-router-dom"
 
